48471c64218b5b55d2609e3205adc4c8dde72703
ShorenaK <shorenaanzhilov@gmail.com>
2024-03-24 19:35:01 -0400

    verable added

--- MyFirstJavaApp.java
+++ MyFirstJavaApp.java
@@ -1,7 +1,30 @@
 public class MyFirstJavaApp {
 
     public static void main(String[] args){
-        System.out.println("Hello!");
+       int numberOfFollowers;
+       numberOfFollowers = 195;
+       byte numberOfSeats = 5;      
+       byte numberOfDoors = 3;      
+       byte numberOfVehiclesOwners = 1;      
+       short power = 36;
+       short housePower = 36;
+       short co2Emission = 333;
+
+       int pice = 29999;
+       int mileage = 14999;
+
+       long registration = 239090090090L;
+       float fuelConsuptioned = 15.5F;
+       float fuelConsuptionedUrbent = 21.4f;
+
+       double fuelConsuptionedAverage = 152.345662e-1;
+       boolean isDamaged = true;
+       char eneregyEfficiancyCat = 'G';
+
+       System.out.println("2018 Doge Challenger SRT 392");
+       System.out.println("Peoce: " + pice + "euro");
+       System.out.println("Mileage: " + mileage + "euro");
+       System.out.println("Mileage: " + registration + "euro");
 
     }
 }
